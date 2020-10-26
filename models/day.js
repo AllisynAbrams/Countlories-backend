1a7d8c536d6ebe6554cded9e0770a26971f732a3
Lien Nguyen <lien@Liens-MacBook-Air.local>
2020-10-26 09:04:46 -0700

    change the day schema, date type

--- models/day.js
+++ models/day.js
@@ -2,7 +2,7 @@ const mongoose = require('../db/connection')
 const Schema = mongoose.Schema
 
 const daySchema = new Schema({
-    date:  String,
+    date:  {type: Date, default: Date.now},
     time: String,
     food: [
         { ref: 'Food', type: Schema.Types.ObjectId}
